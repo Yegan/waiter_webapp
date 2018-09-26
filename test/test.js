bc056d18561cbea63d02cf479fc33b36e86a812e
Yegan <yeganj@gmail.com>
2018-09-26 10:39:26 +0200

    test passing for days of the week

--- test/test.js
+++ test/test.js
@@ -16,11 +16,37 @@ describe('Waiter Web-App', function () {
     await pool.query('delete from days_of_the_week')
     await pool.query('delete from shift_days')
 
+    // inserting the days of the week into days_of_the_week table
+
     await pool.query('insert into days_of_the_week(days_of_week) values($1)', ['Monday'])
+    await pool.query('insert into days_of_the_week(days_of_week) values($1)', ['Tuesday'])
+    await pool.query('insert into days_of_the_week(days_of_week) values($1)', ['Wednesday'])
+    await pool.query('insert into days_of_the_week(days_of_week) values($1)', ['Thursday'])
+    await pool.query('insert into days_of_the_week(days_of_week) values($1)', ['Friday'])
+    await pool.query('insert into days_of_the_week(days_of_week) values($1)', ['Saturday'])
+    await pool.query('insert into days_of_the_week(days_of_week) values($1)', ['Sunday'])
   })
+  it('The function should bring back all the days of the week from the table ', async function () {
+    const weekDays = [ 'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+      'Sunday' ]
 
+    let funcWaiter = waiterFunc(pool)
 
-  describe('')
+    let daysOfTheWeek = await funcWaiter.daysOfTheWeek()
+
+    const daysInDatabase = daysOfTheWeek.map((r) => r.days_of_week)
+    console.log(daysInDatabase)
+
+    assert.deepEqual(daysInDatabase, weekDays)
+
+    // look at lists every and includes function
+
+  })
 
   after(async function () {
     await pool.end()
--- waiterFunc.js
+++ waiterFunc.js
@@ -1,7 +1,9 @@
 module.exports = function (pool) {
+
   async function daysOfTheWeek () {
-    let weekDays = pool.query('select * from days_of_the_week')
-    return weekDays
+    let weekDays = await pool.query('select days_of_week from days_of_the_week')
+    console.log(weekDays)
+    return weekDays.rows
   }
 
   return {
